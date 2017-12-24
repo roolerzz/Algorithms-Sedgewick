54c4e118d2e92dd44dfdf8110bac09f69fbea920
Hemant Sethi <sethi.hemant@gmail.com>
2017-12-23 22:37:28 -0500

    Added the QuickUnion Algorithm for dynamic connectiviy problem.

--- src/com/algorithm/dynamicconnectivity/ClientProgram.java
+++ src/com/algorithm/dynamicconnectivity/ClientProgram.java
@@ -2,8 +2,8 @@ package com.algorithm.dynamicconnectivity;
 public class ClientProgram {
 
 	public static void main(String[] args){
-		QuickFindUF obj = new QuickFindUF(10);
-		obj.printArray();
+		QuickUnionUF obj = new QuickUnionUF(10);
+		/*obj.printArray();
 		obj.union(4,3);
 		obj.union(3,8);
 		obj.union(6,5);
@@ -13,6 +13,11 @@ public class ClientProgram {
 		System.out.println(obj.find(5,0));
 		obj.union(5,0);
 		obj.union(6,1);
-		System.out.println(obj.find(5,0));
+		System.out.println(obj.find(5,0));*/
+		obj.union(3, 4);
+		obj.union(4, 9);
+		obj.union(2, 9);
+		obj.union(5, 6);
+		
 	}
 }
---
+++ src/com/algorithm/dynamicconnectivity/QuickUnionUF.java
@@ -0,0 +1,47 @@
+package com.algorithm.dynamicconnectivity;
+
+public class QuickUnionUF{
+
+	// Solving the problem of Dynamic connectivity of N Objects. Objects are modeled as 
+	// numbers from 0 to N-1, for each object, value in the array depicts its connection to other objects. 
+	// Array represents a set of trees, where each tree is a connected component set. value for an ID represents its parent
+
+	public int[] id;
+
+	QuickUnionUF(int n){
+		id = new int[n];
+		for (int i=0; i<n;i++){
+			id[i]=i;
+		}
+	}
+
+	// Find if element p is connected to element q. O(1) Time complexity.
+	public boolean find(int p, int q){
+		return root(p)==root(q);
+	}
+
+	public int root(int root){
+		int length = id.length;
+		while((root>0 && root<length) && root != id[root]){
+		root = id[root];
+		}
+		return root;
+	}
+
+	// make a connection from p to q.
+	public void union(int p, int q){
+	// Change the value of all the connected components from P to value at Q.
+		id[root(p)] = id[root(q)];
+		printArray();
+	}
+
+	public void printArray(){
+		System.out.println();
+		int length = id.length;	
+		for(int i=0;i<length;i++){
+			System.out.print(id[i]+ ":");
+		}
+	}
+
+
+}
