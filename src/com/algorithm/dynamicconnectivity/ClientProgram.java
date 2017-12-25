3498ca4d2c742a3d5a79e138284b2af105815090
Hemant Sethi <sethi.hemant@gmail.com>
2017-12-24 20:41:41 -0500

    Adding Binary Search Algorithm for finding/searching a number.

--- src/com/algorithm/dynamicconnectivity/ClientProgram.java
+++ src/com/algorithm/dynamicconnectivity/ClientProgram.java
@@ -1,27 +1,13 @@
 package com.algorithm.dynamicconnectivity;
 
+import com.algorithm.searching.BinarySearchInSortedArray;
+
 public class ClientProgram {
 	public static void main(String[] args){
-		QuickUnionWeightedUF obj = new QuickUnionWeightedUF(10);
-		obj.printArray(obj.id);
-		System.out.println("Print Size");
-		obj.printArray(obj.size);	
-		obj.union(4,3);
-		obj.union(3,8);
-		obj.union(6,5);
-		obj.union(9,4);
-		obj.union(2,1);
-		System.out.println("1st Print");
-		obj.printArray(obj.id);
-		System.out.println(obj.find(8,9));
-		System.out.println(obj.find(5,0));
-		obj.union(5,0);
-		obj.union(7,2);
-		obj.union(6,1);
-		obj.union(7,3);
-		System.out.println("2nd Print");
-		obj.printArray(obj.id);	
-		System.out.println("Print Size");
-		obj.printArray(obj.size);
+
+	BinarySearchInSortedArray bs = new BinarySearchInSortedArray();
+	System.out.println("Number of elements in array : " + bs.getArray().length);
+	bs.findKey(212);	
+	bs.findKey(-253);
 	}
 }
---
+++ src/com/algorithm/searching/BinarySearchInSortedArray.java
@@ -0,0 +1,38 @@
+package com.algorithm.searching;
+
+public class BinarySearchInSortedArray{
+
+	private int[] arr;
+
+	public int[] getArray(){
+		return arr;
+	}	
+
+	public BinarySearchInSortedArray(){
+		arr = new int[] {10,35,51,75,81,89,123,149,189,200,211,253};	
+	}
+	
+	public boolean findKey(int key){
+		int lowInd = 0;
+		int highInd = arr.length-1;
+		int iterations= 0;
+		while(lowInd<=highInd){
+			iterations++;
+			int midInd = lowInd + (highInd-lowInd+1)/2;	
+			if(arr[midInd]==key){
+			System.out.println("Key : "+key + " Found");
+			System.out.println("Iterations if key found:" + iterations);
+			return true;
+			}
+			else if (key<arr[midInd]){
+			highInd = midInd -1;
+			}
+			else {
+			lowInd = midInd + 1;
+			}			
+		}	
+		System.out.println("Key : " + key + " not present in the array.");
+		System.out.println("Iterations if key not found:" + iterations);
+		return false;
+	}
+}
