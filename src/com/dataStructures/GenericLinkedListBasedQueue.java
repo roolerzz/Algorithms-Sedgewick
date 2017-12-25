a2c30c9437d6157b94036c7fc8f452605166b70d
Hemant Sethi <sethi.hemant@gmail.com>
2017-12-25 18:58:53 -0500

    Adding Generic Versions of Array and Linked List based implementation of Stacks and Queue.

---
+++ src/com/dataStructures/GenericLinkedListBasedQueue.java
@@ -0,0 +1,86 @@
+package com.dataStructures;
+
+public class GenericLinkedListBasedQueue<Item>{
+	//FIFO. Need 2 variables 1 to track first node and 1 to track last. First is where deletion(dequeue) would be done and last is where addition(enqueue).	
+
+	private Node first = null; 
+
+	private Node last = null;
+
+	private class Node {
+		Item item;
+		Node next;		
+	}
+	
+	// insert at the last of the linked list
+	public void enqueue(Item obj){
+		Node oldLast = last;
+		last = new Node();
+		last.item=obj;
+		
+		if(isEmpty()){
+			first = last;
+			}
+		else
+			oldLast.next=last;
+	}
+	
+	// remove from the start of the queue.
+	public void dequeue(){
+		if(isEmpty())
+			{
+			System.out.println("Cannot delete from an empty Queue.");
+			return;
+			}
+		System.out.print(first.item + " ");
+		first = first.next;
+		if(isEmpty()) {
+			last = null;
+		}
+	}
+	
+	public boolean isEmpty(){
+		return first==null;
+	}
+	
+	/*@Override
+	public String toString() {
+		String textToPrint= "Empty Stack";
+			if (first == null)
+				return textToPrint;
+			Node traversalNode = first;
+			textToPrint = "";
+			while(traversalNode.next!=null) {
+				textToPrint= textToPrint.concat(traversalNode.item.toString());
+				textToPrint = textToPrint.concat(" ");
+				traversalNode = traversalNode.next;
+			}
+			textToPrint= textToPrint.concat(traversalNode.item.toString());
+		return textToPrint;
+	}*/
+}
+
+
+/*public class ClientProgram {
+	public static void main(String[] args){
+	LinkedListQueueOfStrings queue = new LinkedListQueueOfStrings();
+	queue.enqueue("to");
+	queue.enqueue("be");
+	queue.enqueue("or");
+	queue.enqueue("not");
+	queue.enqueue("to");
+	queue.dequeue();
+	queue.enqueue("be");
+	queue.dequeue();
+	queue.dequeue();
+	queue.enqueue("that");
+	queue.dequeue();
+	queue.dequeue();
+	queue.dequeue();
+	queue.enqueue("is");
+	queue.dequeue();
+	queue.dequeue();
+	queue.dequeue();
+	queue.dequeue();
+	}
+}*/
---
+++ src/com/dataStructures/GenericResizeableArrayBasedQueue.java
@@ -0,0 +1,137 @@
+package com.dataStructures;
+
+public class GenericResizeableArrayBasedQueue<Item>{
+	// Earlier we needed to provide the size of the array before hand, to
+	// initialize, which is kind of the restriction in cases, where client
+	// does not know what is number of elements its gonno insert in the stack. To
+	// work with that limitation, we need to resize the array.
+	// Everytime in case of push operation, if the stack is full, create a new array
+	// with double the size, copy the existing contents onto the new
+	// and return the array. In case of pop, operation, to optimize that alot of
+	// space is not wasted, everytime the array is 1/4 of its size full,
+	// create a new array with half the size and move elements to that.
+
+	private Item[] arr = null;
+
+	private int head = 0;
+
+	private int tail = 0;
+
+	public GenericResizeableArrayBasedQueue() {
+		arr = (Item[])new Object[1];
+	}
+
+	public void enqueue(Item obj) {
+		int length = arr.length;
+		if ((tail - head) == length) {
+			resize(2 * length);
+		} else if (tail == length) {
+			moveArrayBack();
+		}
+		arr[tail++] = obj;
+
+	}
+
+	private void moveArrayBack() {
+		System.out.println("Array Moved Back");
+		int counter = head;
+		int numelem = tail - head;
+		for (int i = 0; i < (numelem); i++) {
+			arr[i] = arr[counter];
+			arr[counter] = null;
+			counter++;
+		}
+		head = 0;
+		tail = head + numelem;
+	}
+
+	private void resize(int newlen) {
+		moveArrayBack();
+		System.out.println("Resizing the array from : " + arr.length + " to :" + newlen);
+		Item[] newArray = (Item[])new Object[newlen];
+		for (int i = 0; i < tail; i++) {
+			newArray[i] = arr[i];
+		}
+		arr = newArray;
+	}
+
+	// edge case, taking care of head when there's only 1 element.
+	public void dequeue() {
+		if (isEmpty()) {
+			System.out.println("Queue Underflow");
+			return;
+		}
+		if ((tail - head) <= arr.length / 4) {
+			resize(arr.length / 2);
+		}
+		System.out.print(arr[head] + " ");
+		arr[head++] = null;
+		if (isEmpty()) {
+			head = 0;
+			tail = 0;
+		}
+	}
+
+	public boolean isEmpty() {
+		return (tail - head) == 0;
+	}
+
+	public void printArray() {
+		System.out.println("Head is : " + head);
+		System.out.println("Tail is : " + tail);
+		System.out.print("[");
+		for (int i = 0; i < arr.length - 1; i++) {
+			System.out.print(arr[i] + ",");
+		}
+		System.out.print(arr[arr.length - 1]);
+		System.out.println("]");
+	}
+}
+
+/*public class ClientProgram {
+	public static void main(String[] args) {
+		ResizeArrayQueue queue = new ResizeArrayQueue();
+		queue.enqueue("to");
+		queue.printArray();
+		queue.enqueue("be");
+		queue.printArray();
+		queue.dequeue();
+		queue.printArray();
+		queue.dequeue();
+		queue.printArray();
+		queue.enqueue("to");
+		queue.printArray();
+		queue.enqueue("or");
+		queue.printArray();
+		queue.enqueue("not");
+		queue.printArray();
+		queue.enqueue("to");
+		queue.printArray();
+		queue.dequeue();
+		queue.printArray();
+		queue.enqueue("be");
+		queue.printArray();
+		queue.dequeue();
+		queue.printArray();
+		queue.dequeue();
+		queue.printArray();
+		System.out.println("Before inserting that");
+		queue.enqueue("that");
+		queue.printArray();
+		System.out.println("After inserting that");
+		queue.dequeue();
+		queue.printArray();
+		queue.dequeue();
+		queue.printArray();
+		queue.dequeue();
+		queue.printArray();
+		queue.enqueue("is");
+		queue.printArray();
+		queue.dequeue();
+		queue.printArray();
+		queue.dequeue();
+		queue.dequeue();
+		queue.dequeue();
+		queue.printArray();
+	}
+}*/
