c9eca26b3acd4fcce232bdfb549706f842f06050
Hemant Sethi <sethi.hemant@gmail.com>
2017-12-25 14:06:32 -0500

    Adding Array(Non-Resizeable) implementation of Stack.

---
+++ src/com/dataStructures/ArrayStackOfStrings.java
@@ -0,0 +1,56 @@
+package com.dataStructures;
+
+public class ArrayStackOfStrings{
+
+	private String[] arr = null;
+
+	private int top=0;
+
+	public ArrayStackOfStrings(int n){
+		arr = new String[n];
+	}
+	
+	public void push(String obj){
+		int length = arr.length;
+		if(top<length){
+		arr[top++] = obj;}
+		else {
+		System.out.println("Stack Overflow");
+		}
+	}
+	
+
+	public void pop(){
+		if(isEmpty()) {System.out.println("Stack Underflow"); return;}
+		arr[top] = null;
+		System.out.print(arr[--top] + " ");
+	}
+	
+	public boolean isEmpty(){
+		return top==0;
+	}
+
+}
+
+
+/*public class ClientProgram {
+	public static void main(String[] args){
+	ArrayStackOfStringsstack = new ArrayStackOfStrings();
+	System.out.println(stack);
+	stack.push("to");
+	stack.push("be");
+	stack.push("or");
+	stack.push("not");
+	stack.push("to");
+	stack.pop();
+	stack.push("be");
+	stack.pop();
+	stack.pop();
+	stack.push("that");
+	stack.pop();
+	stack.pop();
+	stack.pop();
+	stack.push("is");
+	System.out.println(stack);
+	}
+}*/
