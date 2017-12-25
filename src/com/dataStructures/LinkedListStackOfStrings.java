145b7e20fb96f5a9c3799d5bf6ae136abb8ae06d
Hemant Sethi <sethi.hemant@gmail.com>
2017-12-25 13:54:04 -0500

    Adding Linked List Implementation of Stack of String. Up Next: Array Implementation.

---
+++ src/com/dataStructures/LinkedListStackOfStrings.java
@@ -0,0 +1,77 @@
+package com.dataStructures;
+
+public class LinkedListStackOfStrings{
+	
+	private Node first = null; 
+
+	private class Node {
+		String item;
+		Node next;		
+	}
+	
+	public void push(String obj){
+		if(isEmpty()){
+			first = new Node();
+			first.item=obj;
+			return;
+		}
+			Node oldFirst = first;
+			first = new Node();
+			first.item=obj;
+			first.next=oldFirst;
+	}
+	
+
+	public void pop(){
+		if(isEmpty())
+			{
+			System.out.println("Cannot delete from an empty Stack. Stack Underflow");
+			return;
+			}
+		System.out.print(first.item + " ");
+		first = first.next;
+	}
+	
+	public boolean isEmpty(){
+		return first==null;
+	}
+	
+	/*@Override
+	public String toString() {
+		String textToPrint= "Empty Stack";
+			if (first == null)
+				return textToPrint;
+			Node traversalNode = first;
+			textToPrint = "";
+			while(traversalNode.next!=null) {
+				textToPrint= textToPrint.concat(traversalNode.item.toString());
+				textToPrint = textToPrint.concat(" ");
+				traversalNode = traversalNode.next;
+			}
+			textToPrint= textToPrint.concat(traversalNode.item.toString());
+		return textToPrint;
+	}*/
+}
+
+
+/*public class ClientProgram {
+	public static void main(String[] args){
+	LinkedListStackOfStrings stack = new LinkedListStackOfStrings();
+	System.out.println(stack);
+	stack.push("to");
+	stack.push("be");
+	stack.push("or");
+	stack.push("not");
+	stack.push("to");
+	stack.pop();
+	stack.push("be");
+	stack.pop();
+	stack.pop();
+	stack.push("that");
+	stack.pop();
+	stack.pop();
+	stack.pop();
+	stack.push("is");
+	System.out.println(stack);
+	}
+}*/
