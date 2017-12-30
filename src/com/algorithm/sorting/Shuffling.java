98ba98d112952a7bd18182da1bfc2bae0b3505d0
Hemant Sethi <sethi.hemant@gmail.com>
2017-12-29 20:50:32 -0500

    Added khuth Shuffle for linear time shuffling.

--- src/com/algorithm/sorting/Shuffling.java
+++ src/com/algorithm/sorting/Shuffling.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class Shuffling {
 
+	// take time as per the sorting algorithm used.
 	public void shuffle(Comparable[] arr){
 		int N = arr.length;
 		Random generator = new Random();
@@ -13,12 +14,22 @@ public class Shuffling {
 		}
 		shuffleSort(arr,rand);
 	}
+	
+	// Takes linear time.
+	public void knuthShuffle(Comparable[] arr){
+		int N = arr.length;
+		Random generator = new Random();
+		for(int i=0; i<N;i++){
+			int r = generator.nextInt(i+1);
+			SortUtil.exchange(arr,i,r);
+		}
+	}
 
 	// Used insertion sort to sort an array of randomly generated numbers and accordingly shuffled the normal deck of cards.
 	private void shuffleSort(Comparable[] arr,Comparable[] rand){
 		int len = arr.length;
 		/*System.out.println("Before Shuffling");*/
-		printArrays(arr, rand, len);
+		//printArrays(arr, rand, len);
 		for(int k = 0; k<len-1 ; k++){
 			for(int j = k+1; j>0; j--){
 				if(SortUtil.less(rand[j],rand[j-1])){
@@ -28,17 +39,7 @@ public class Shuffling {
 			}
 		}
 		/*System.out.println("After Shuffling");*/
-		printArrays(arr, rand, len);
+		//printArrays(arr, rand, len);
 	}
 
-	private void printArrays(Comparable[] arr, Comparable[] rand, int len) {
-		/*for(int i=0;i<len;i++) {
-			System.out.print(": " + rand[i]);
-		}*/
-		System.out.println();
-		for(int i=0;i<len;i++) {
-			System.out.print(": " + arr[i]);
-		}
-		
-	}
 }
