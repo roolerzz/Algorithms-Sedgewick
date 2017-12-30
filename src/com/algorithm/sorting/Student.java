4f959c2f683801fe8fca23f82682b33d5e0b1a3b
Hemant Sethi <sethi.hemant@gmail.com>
2017-12-30 15:28:57 -0500

    Adding Commented Client for future reference.

--- src/com/algorithm/sorting/Student.java
+++ src/com/algorithm/sorting/Student.java
@@ -1,5 +1,6 @@
 package com.algorithm.sorting;
 
+import java.util.Arrays;
 import java.util.Comparator;
 
 public class Student {
@@ -57,6 +58,27 @@ public class Student {
 			return o1.rollNo-o2.rollNo;
 		}
 	}
-	
+	/*public class SortingClient {
+
+		public static void main(String[] args){
+			Student[] arr = new Student[20];
+			String[] random = {"C","S","D","Z","T","C","S","D","Z","T","C","S","D","Z","T","C","S","D","Z","T"};
+			for(int i=0;i<20;i++) {
+				ComparableDate dob = new ComparableDate(1991,(i+1),1);
+				Student student = new Student((i+1), random[i]+(i+1),dob);
+				arr[i] = student;
+			}
+			
+			System.out.println("Comparing DOB");
+			Arrays.sort(arr, Student.DOB_COMPARATOR);
+			SortUtil.printArray(arr);
+			System.out.println("Comparing Names");
+			Arrays.sort(arr,Student.NAME_COMPARATOR);
+			SortUtil.printArray(arr);
+			System.out.println("Comparing Roll Numbers");
+			Arrays.sort(arr,Student.ROLL_COMPARATOR);
+			SortUtil.printArray(arr);
+			
+		}*/
 	
 }
