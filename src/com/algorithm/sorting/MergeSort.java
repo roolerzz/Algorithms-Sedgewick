c857204f3d5f68c2b668be249eb051736ee6ecad
Hemant Sethi <sethi.hemant@gmail.com>
2017-12-29 22:22:46 -0500

    Adding Implementation of Merge Sort.O(NlogN) runtime.

---
+++ src/com/algorithm/sorting/MergeSort.java
@@ -0,0 +1,38 @@
+package com.algorithm.sorting;
+
+public class MergeSort {
+
+	public void sort(Comparable[] arr) {
+		Comparable[] aux = new Comparable[arr.length];
+		sort(arr, aux , 0, arr.length-1);
+	}
+
+	private void sort(Comparable[] arr, Comparable[] aux ,int low, int high) {
+		int mid = low + (high - low) / 2;
+		if (low >= high)
+			return;
+		sort(arr, aux, low, mid);
+		sort(arr, aux ,mid + 1, high);
+		merge(arr, aux ,low, mid, high);
+	}
+
+	private void merge(Comparable[] arr, Comparable[] aux, int low, int mid, int high) {
+		// copying to auxilliary array before merging.
+		int i = low;
+		int j = mid + 1;
+		for(int k = low; k<=high ; k++) {
+			aux[k] = arr[k];
+		}
+		for (int k = low; k <= high; k++) {
+			if (i >= j)
+				arr[k] = aux[j++];
+			else if (j > high)
+				arr[k] = aux[i++];
+			else if (SortUtil.less(aux[j], aux[i]))
+				arr[k] = aux[j++];
+			else arr[k] = aux[i++];
+		}
+
+	}
+
+}
--- src/com/algorithm/sorting/SortingClient.java
+++ src/com/algorithm/sorting/SortingClient.java
@@ -2,29 +2,20 @@ package com.algorithm.sorting;
 
 public class SortingClient {
 
-	public static void main(String[] args) {
-		Comparable[] arr = new Comparable[12*31];
-		
-		int counter = 0;
-		for(int i = 12 ; i >= 1 ; i--) {
-			for (int j = 31; j >=1 ; j--) {
-				ComparableDate date = new ComparableDate(1991,i,j);
-				arr[counter++] = date;
+		public static void main(String[] args){
+			Integer[] deck = new Integer[10];
+			int len=deck.length;
+			for(int i=0; i<len ; i++){
+				deck[i] =(i+1);
 			}
+			
+			Shuffling shuffling = new Shuffling();
+			SortUtil.printArray(deck);
+			shuffling.knuthShuffle(deck);
+			SortUtil.printArray(deck);
+			MergeSort merge = new MergeSort();
+			merge.sort(deck);
+			SortUtil.printArray(deck);
 		}
-		Comparable[] arr2  = new Comparable[12*31];
-		int counter2= 0;
-		for(int i = 12 ; i >= 1 ; i--) {
-			for (int j = 31; j >=1 ; j--) {
-				ComparableDate date = new ComparableDate(1991,i,j);
-				arr2[counter2++] = date;
-			}
-		}
-		
-		InsertionSort insertion = new InsertionSort(); 
-		SelectionSort selection= new SelectionSort();
-		insertion.sort(arr);
-		selection.sort(arr2);
-	}
 
 }
