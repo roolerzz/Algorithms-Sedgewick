f1df54d42eb07297d03c38f453ecb25a1a7b01d7
Hemant Sethi <sethi.hemant@gmail.com>
2017-12-31 22:30:34 -0500

    Updated SortUtil to support greater comparison plus formatting done on file.

--- src/com/algorithm/sorting/SortUtil.java
+++ src/com/algorithm/sorting/SortUtil.java
@@ -1,31 +1,38 @@
 package com.algorithm.sorting;
 
-public class SortUtil{
+public class SortUtil {
 
-	public static void exchange(Comparable[] arr, int i, int j){
+	public static void exchange(Comparable[] arr, int i, int j) {
 		Comparable obj = arr[i];
 		arr[i] = arr[j];
 		arr[j] = obj;
 	}
-	
-	public static boolean less(Comparable a, Comparable b){
+
+	public static boolean less(Comparable a, Comparable b) {
 		// A's compareTo method should throw exception in case of incompatible objects.
 		return a.compareTo(b) < 0;
-	}	
+	}
+
+	public static boolean greater(Comparable a, Comparable b) {
+		return a.compareTo(b) > 0;
+	}
+
 	public static void printArrayComparable(Comparable[] arr) {
 		int len = arr.length;
 		System.out.println();
-		for(int i=0; i<len;i++) {
+		for (int i = 0; i < len; i++) {
 			System.out.print(":" + arr[i]);
 		}
 	}
+
 	public static void printArray(StudentComparator[] arr) {
 		int len = arr.length;
 		System.out.println();
-		for(int i=0; i<len;i++) {
-			System.out.print(" Name :" + arr[i].getName() + " Roll :" + arr[i].getRollNo() + " DOB :" + arr[i].getDob());
+		for (int i = 0; i < len; i++) {
+			System.out
+					.print(" Name :" + arr[i].getName() + " Roll :" + arr[i].getRollNo() + " DOB :" + arr[i].getDob());
 		}
 		System.out.println();
 	}
-	
+
 }
