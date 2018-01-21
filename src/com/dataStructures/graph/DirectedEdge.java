b411b4ee5892125c09c136026d4da7c34e35acf1
Hemant Sethi <sethi.hemant@gmail.com>
2018-01-20 21:45:50 -0500

    Fixed bugs.

--- src/com/dataStructures/graph/DirectedEdge.java
+++ src/com/dataStructures/graph/DirectedEdge.java
@@ -7,6 +7,9 @@ public class DirectedEdge implements Comparable<DirectedEdge>{
 	private final double weight;
 	
 	public DirectedEdge(int v, int w, double weight) {
+		if (v < 0) throw new IllegalArgumentException("Vertex names must be nonnegative integers");
+        if (w < 0) throw new IllegalArgumentException("Vertex names must be nonnegative integers");
+        if (Double.isNaN(weight)) throw new IllegalArgumentException("Weight is NaN");
 		this.v = v;
 		this.w = w;
 		this.weight = weight;
@@ -22,6 +25,11 @@ public class DirectedEdge implements Comparable<DirectedEdge>{
 	public int to() {
 		return w;
 	}
+	
+	public String toString() {
+        return v + "->" + w + " " + String.format("%5.2f", weight);
+    }
+
 
 	public int compareTo(DirectedEdge that) {
 		return Double.compare(this.weight,that.weight);
--- src/com/dataStructures/graph/EdgeWeightedDigraph.java
+++ src/com/dataStructures/graph/EdgeWeightedDigraph.java
@@ -4,21 +4,27 @@ import edu.princeton.cs.algs4.Bag;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.Queue;
 import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdOut;
 
 // Allows self loops and parallel edges.
 public class EdgeWeightedDigraph {
 
+	private static final String NEWLINE = System.getProperty("line.separator");
+	
 	private final int V;
 	
 	private int E;
 	
 	private Bag<DirectedEdge>[] adj;
 	
-	private Queue<DirectedEdge> edges;
+	 private int[] indegree;    
 	
 	// initializes an empty edgeweighteddigraph with internal adjacency list data structure empty-initialized.
 	public EdgeWeightedDigraph(int V) {
+		if (V < 0) throw new IllegalArgumentException("Number of vertices in a Digraph must be nonnegative");
 		this.V = V;
+		this.E=0;
+		this.indegree = new int[V];
 		adj = (Bag<DirectedEdge>[])new Bag[V];
 		for(int v = 0 ; v < V ; v++) {
 			adj[v]= new Bag<DirectedEdge>();
@@ -26,26 +32,31 @@ public class EdgeWeightedDigraph {
 	}
 	
 	public EdgeWeightedDigraph(In in) {
-		this.V = in.readInt();
-		this.E = in.readInt();
-		while(in.hasNextLine()) {
+		this(in.readInt());
+		int e2 = in.readInt();
+		if (e2 < 0) throw new IllegalArgumentException("Number of edges must be nonnegative");
+		for (int i = 0; i < e2; i++) {
 			int v = in.readInt();
 			int w = in.readInt();
 			double weight = in.readDouble();
-			DirectedEdge e = new DirectedEdge(v,w,weight);
-			addEdge(e);
+			GraphHelper.validateVertex(v, V);
+			GraphHelper.validateVertex(w, V);
+			addEdge(new DirectedEdge(v,w,weight));
 		}
 	}
 	
 	// created a deep copy of a given edge weighted Digraph.
 	public EdgeWeightedDigraph(EdgeWeightedDigraph G) {
-		this(G.V());
+		this(G.V()); 
+		this.E = G.E();
+		 for (int v = 0; v < G.V(); v++)
+	            this.indegree[v] = G.indegree(v);
 		for(int v = 0 ; v < G.V(); v++) {
-			Stack<DirectedEdge> stack = new Stack<>();
+			Stack<DirectedEdge> reverse = new Stack<>();
 			for(DirectedEdge e : G.adj(v)) {
-				stack.push(e);
+				reverse.push(e);
 			}
-			for(DirectedEdge e : stack) {
+			for(DirectedEdge e : reverse) {
 				adj[v].add(e);
 			}
 		}
@@ -57,6 +68,7 @@ public class EdgeWeightedDigraph {
 		GraphHelper.validateVertex(v, V);
 		GraphHelper.validateVertex(w, V);
 		adj[v].add(e);
+		indegree[w]++;
 		E++;
 	}
 	
@@ -73,13 +85,40 @@ public class EdgeWeightedDigraph {
 		return E;
 	}
 	
+	 public int indegree(int v) {
+	        GraphHelper.validateVertex(v,V);
+	        return indegree[v];
+	    }
+	  
+	 public int outdegree(int v) {
+		 GraphHelper.validateVertex(v,V);
+	        return adj[v].size();
+	    }
+	
 	public Iterable<DirectedEdge> edges(){
-		edges = new Queue<>();
-		for(Bag<DirectedEdge> b : adj) {
-			for(DirectedEdge e : b) {
-				edges.enqueue(e);
+		Bag<DirectedEdge> list = new Bag<DirectedEdge>();
+		  for (int v = 0; v < V; v++) {
+			  for (DirectedEdge e : adj(v)) {
+				  list.add(e);
 			}
 		}
-		return edges;
+		return list;
 	}
+	 public String toString() {
+	        StringBuilder s = new StringBuilder();
+	        s.append(V + " " + E + NEWLINE);
+	        for (int v = 0; v < V; v++) {
+	            s.append(v + ": ");
+	            for (DirectedEdge e : adj[v]) {
+	                s.append(e + "  ");
+	            }
+	            s.append(NEWLINE);
+	        }
+	        return s.toString();
+	    }
+	 public static void main(String[] args) {
+	        In in = new In(args[0]);
+	        EdgeWeightedDigraph G = new EdgeWeightedDigraph(in);
+	        StdOut.println(G);
+	    }
 }
